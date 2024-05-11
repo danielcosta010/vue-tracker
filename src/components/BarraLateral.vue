00d15a8d756df0f213f24395ca272c2234f586ad
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-05-11 01:18:26 -0300

    Create BarraLateral.vue

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <h1>
+      <img src="../assets/logo.png" alt="">
+    </h1>
+
+  </header>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: "BarraLateral",
+  })
+</script>
+
+<style escoped>
+  header{
+    background-color: rgb(55, 55, 93);
+    width: 100%;
+    height: 100vh;
+  }
+  @media only screen and (max-width: 768px) {
+    header{
+      padding: 2.5rem;
+      height: auto;
+    }
+  }
+</style>
