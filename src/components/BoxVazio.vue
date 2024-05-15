0986c56b7f712b8eac533aad295770e92095b99b
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-05-15 00:55:24 -0300

    Create BoxVazio.vue

---
+++ src/components/BoxVazio.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="box has-text-wieght-bold">
+    <slot></slot>
+  </div>  
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent( {
+  name: 'BoxVazio',
+})
+</script>
+
+<style scoped>
+.box {
+  background: #FAF0CA;
+}
+</style>
