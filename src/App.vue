24dd20613c83eee6b594c4d6e2ffa048782e8ada
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-05-15 00:56:11 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <main class="columns is-gapless is-multiline">
+  <main class="columns is-gapless is-multiline" :class="{ 'modo-escuro': modoEscuroAtivo }">
     <div class="column is-one-quarter">
-      <BarraLateral />
+      <BarraLateral @mudouTema="trocaTema" />
     </div>
-    <div class="column is-three-quarter">
-      <FormCount @aoSalvarTarefa="salvarTarefa"/>
+    <div class="column is-three-quarter conteudo">
+      <FormCount @aoSalvarTarefa="salvarTarefa" />
       <div class="lista">
-        <TarefaConcluida v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa"/>
-        
+        <TarefaConcluida v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+        <BoxVazio v-if="listaVazia">
+          Você ainda não realizou nenhuma tarefa.
+        </BoxVazio>
       </div>
     </div>
   </main>
-  
+
 </template>
 
 <script lang="ts">
@@ -20,7 +22,7 @@ import BarraLateral from './components/BarraLateral.vue';
 import FormCount from './components/FormCount.vue';
 import TarefaConcluida from './components/TarefaConcluida.vue';
 import ITarefa from './interfaces/ITarefa';
-
+import BoxVazio from './components/BoxVazio.vue';
 
 
 export default defineComponent({
@@ -28,23 +30,46 @@ export default defineComponent({
   components: {
     BarraLateral,
     FormCount,
-    TarefaConcluida
+    TarefaConcluida,
+    BoxVazio
   },
   data() {
     return {
-      tarefas: [] as ITarefa[]
+      tarefas: [] as ITarefa[],
+      modoEscuroAtivo: false
+    }
+  },
+  computed: {
+    listaVazia(): boolean {
+      return this.tarefas.length === 0
     }
   },
   methods: {
     salvarTarefa(tarefa: ITarefa) {
       this.tarefas.push(tarefa);
+    },
+    trocaTema(modoEscuroAtivo: boolean) {
+      this.modoEscuroAtivo = modoEscuroAtivo;
     }
   }
 });
 </script>
 
-<style >
+<style>
 .lista {
   padding: 1.25rem;
+}
+
+/* main {
+  --bg-primario: #fff;
+  --texto-primario: #000;
+} */
+.modo-escuro {
+  --bg-primario: #2b2d42;
+  --texto-primario: #ddd;
+}
+
+.conteudo {
+  background-color: var(--bg-primario);
 }
 </style>
