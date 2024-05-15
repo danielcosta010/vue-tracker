29cfff930937761cb7c3e51b105d97554cb8788d
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-05-15 00:55:35 -0300

    Update TarefaConcluida.vue

--- src/components/TarefaConcluida.vue
+++ src/components/TarefaConcluida.vue
@@ -1,26 +1,28 @@
 <template>
-  <div class="box has-text-wieght-bold">
-    <div class="columns">
-      <div class="column is-7">
-        {{ tarefa.descricao }}
+  <BoxVazio>
+      <div class="columns">
+        <div class="column is-7">
+          {{ tarefa.descricao || 'Tarefa sem descrição' }}
+        </div>
+        <div class="column">
+          <CronometroTempo :contador="tarefa.duracaoEmSegundos"/>
+        </div>
       </div>
-      <div class="column">
-        <CronometroTempo :contador="tarefa.duracaoEmSegundos"/>
-      </div>
-    </div>
-
-  </div>
+  </BoxVazio>
+  
 </template>
 
 <script lang="ts">
 import { PropType, defineComponent } from 'vue';
 import CronometroTempo from './CronometroTempo.vue';
 import ITarefa from '../interfaces/ITarefa';
+import BoxVazio from './BoxVazio.vue';
 
 export default defineComponent({
   name: 'TarefaConcluida',
   components: {
-    CronometroTempo
+    CronometroTempo,
+    BoxVazio
   },
   props: {
     tarefa: {
@@ -31,8 +33,3 @@ export default defineComponent({
 })
 </script>
 
-<style scoped>
-.box {
-  background: #FAF0CA;
-}
-</style>
