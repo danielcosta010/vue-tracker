5c13c9c4fa8d3fbad3b10a31a98dfa81046adc6c
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-05-14 21:06:05 -0300

    Create TarefaConcluida.vue

---
+++ src/components/TarefaConcluida.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="box has-text-wieght-bold">
+    <div class="columns">
+      <div class="column is-7">
+        {{ tarefa.descricao }}
+      </div>
+      <div class="column">
+        <CronometroTempo :contador="tarefa.duracaoEmSegundos"/>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { PropType, defineComponent } from 'vue';
+import CronometroTempo from './CronometroTempo.vue';
+import ITarefa from '../interfaces/ITarefa';
+
+export default defineComponent({
+  name: 'TarefaConcluida',
+  components: {
+    CronometroTempo
+  },
+  props: {
+    tarefa: {
+      type: Object as PropType<ITarefa>,
+      required: true
+    }
+  }
+})
+</script>
+
+<style scoped>
+.box {
+  background: #FAF0CA;
+}
+</style>
